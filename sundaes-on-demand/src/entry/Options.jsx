683c7c52c78fafd0606b23bf68e328ae09ef22a0
alifakbar111 <alifakbar111@gmail.com>
2022-08-17 13:18:45 +0700

    feat: add opton scoop & topping

---
+++ sundaes-on-demand/src/entry/Options.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+
+const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // TODO: to handle error
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+};
+
+export default Options;
